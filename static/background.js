7ae1f576a4b14744cbd14356d16e07bee0546642
Lukas Bockstaller <lukas.bockstaller@posteo.de>
2021-06-24 19:29:10 +0200

    moar content

--- static/background.js
+++ static/background.js
@@ -2,7 +2,7 @@ var backgrounds = [
     {
         keys: ["Kneipe", "Café", "Leuchtfeuer", "Stil und Kultur", "Roverschutzgebiet", "Wespennest"],
         texts: ["Wir suchen für rover<b>voco</b> noch Café und Kneipenteams. Alle Infos gibt es auf <a href='https://voco.rover.de' target='_blank'>voco.rover.de</a>",],
-        images: ["static/background/bialetti.jpg"]
+        images: ["static/background/bialetti.jpg", "static/background/kneipe_1.jpg", "static/background/kneipe.jpg", "static/background/kaffeemaschine.jpg"]
     },
     {
         keys: ["Mülltrennung", "Orga", "Sanis", "Bauhof", "Helfer", "Duschcontainer", "Küchenzelt", "Nachtwache"],
@@ -46,18 +46,18 @@ var backgrounds = [
     },
     {
         keys: ["Bahn", "rover.de",],
-        texts: [],
-        images: []
+        texts: ["Mit den Öffis zum Auftakt und vom Abschluss zurück. Um den Rest kümmern wir uns."],
+        images: ["static/background/zug.jpg", "static/background/bahnhof.jpg"]
     },
     {
         keys: ["Auftakt", "Region",],
-        texts: [],
-        images: []
+        texts: ["rover<b>voco</b> beginnt auf einem Auftakt in deiner Nähe!"],
+        images: ["static/background/zelte_dampf.jpg", "static/background/zelte_dampf_2.jpg"]
     },
     {
         keys: ["Kommunikation", "Neuland"],
-        texts: [],
-        images: []
+        texts: ["Du kannst filmen/fotografieren/gestalten? Ab als Helfer*in ins Kommunikationsteam!"],
+        images: ["static/background/marie_kamera.jpg"]
     },
 
 ]
